5a0f596d37fcb5da0a2fcd8fa87c2d9519e471f9
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2025-01-28 23:05:25 +0100

    Correct syntax error

--- scripts/run_multiple.sh
+++ scripts/run_multiple.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for i in {1..5}
-    for j in {1..10}
+for i in {1..5}; do
+    for j in {1..10}; do
         nohup ./run.sh &> ./tmp/output_seed_$i_noise_$j.log &
-    end
-end
+    done
+done
