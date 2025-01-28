e614c8396bda927cacaeef50588c377e56e63adc
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2025-01-28 23:27:54 +0100

    remove whitespace before and after equal sign

--- scripts/run_multiple.sh
+++ scripts/run_multiple.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 for i in {1..5}; do
-    for j in {1..10}; do
-        pwd
+    for j in {0..10}; do
         nohup ./scripts/run.sh "$i" "$j" &> ./tmp/output_seed_${i}_noise_${j}.log &
     done
 done
