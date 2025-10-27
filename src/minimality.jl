8337413b7e7bd7e81a62a0c4f09817aca627c7da
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2025-10-27 23:20:24 +0100

    Add debug visual

--- src/minimality.jl
+++ src/minimality.jl
@@ -4,6 +4,7 @@ using LinearAlgebra: cross, dot, norm, normalize
 using ..Geometry: Circle, Line
 using ..Space: build_rotation_matrix
 
+using GLMakie: lines! as draw_lines!, Figure, Axis, DataAspect, xlims!, ylims!
 using HomotopyContinuation
 
 function toZero(val)
@@ -21,7 +22,7 @@ function get_tangentpoints_circle_point(circle::Circle, point::Vector{<:Number})
     if d <= r
         throw(TangentLineNotFound("The point is inside the circle"))
     end
-    if (d/r-1) < 1E-8
+    if (d / r - 1) < 1E-8
         throw(TangentLineNotFound("The point is on the circle"))
     end
 
@@ -30,7 +31,7 @@ function get_tangentpoints_circle_point(circle::Circle, point::Vector{<:Number})
     ad = rho^2 * d
     bd = rho * R / d * d
     axis = normalize(circle.axis)
-    
+
     adv = normalize(variation)
     if !isnothing(axis)
         bdv = normalize(cross(axis, adv))
@@ -40,15 +41,15 @@ function get_tangentpoints_circle_point(circle::Circle, point::Vector{<:Number})
 
     T1 = circle.center + ad * adv + bd * bdv
     T2 = circle.center + ad * adv - bd * bdv
-    
-    T1 = rationalize.(toZero.(T1))
-    T2 = rationalize.(toZero.(T2))
+
+    # T1 = rationalize.(toZero.(T1))
+    # T2 = rationalize.(toZero.(T2))
     return T1, T2
 
     throw(TangentLineNotFound("No tangent line possible"))
 end
 
-function build_camera_matrix(intrinsic, rotation, translation; use_rotation_as_is = false, use_translation_as_is = false)
+function build_camera_matrix(intrinsic, rotation, translation; use_rotation_as_is=false, use_translation_as_is=false)
     r₁ = rotation
     if (!use_rotation_as_is)
         r₁ = r₁' # inv(r)
@@ -57,11 +58,13 @@ function build_camera_matrix(intrinsic, rotation, translation; use_rotation_as_i
     if (!use_translation_as_is)
         t₁ = -r₁ * translation
     end
-    return hcat(intrinsic, zeros(Rational, 3)) * vcat(hcat(r₁, t₁), [0 0 0 1])
+    m = hcat(intrinsic, zeros(Rational, 3)) * vcat(hcat(r₁, t₁), [0 0 0 1])
+    return m ./ m[3, 4]
 end
 
 function project_point_into_line(point::Vector{<:Number}, line::Line)::Vector{<:Number}
-    direction = line.direction
+    # direction = rationalize.(line.direction / norm(line.direction))
+    direction = line.direction / norm(line.direction)
     origin = line.origin
     v = point - origin
     return origin + dot(v, direction) * direction
@@ -75,13 +78,10 @@ function get_cylinder_contours(cylinder_axis, camera_position, camera_matrix)
         Circle(circlecenter, radius, cylinder_axis),
         camera_position
     )
-    # display("Tangent points")
-    # display(tangentpoint₁)
-    # display(tangentpoint₂)
-    # display("-------------------")
-    projected_tangentpoint₁ = (camera_matrix * [tangentpoint₁; 1])[1:3]
-    projected_tangentpoint₂ = (camera_matrix * [tangentpoint₂; 1])[1:3]
-    projected_cylinderaxis = (camera_matrix * [cylinder_axis; 0])[1:3]
+
+    projected_tangentpoint₁ = (camera_matrix * [tangentpoint₁; 1])
+    projected_tangentpoint₂ = (camera_matrix * [tangentpoint₂; 1])
+    projected_cylinderaxis = (camera_matrix * [cylinder_axis; 0])
 
     contour₁ = cross(projected_tangentpoint₁, projected_tangentpoint₁ + projected_cylinderaxis)
     contour₂ = cross(projected_tangentpoint₂, projected_tangentpoint₂ + projected_cylinderaxis)
@@ -89,7 +89,7 @@ function get_cylinder_contours(cylinder_axis, camera_position, camera_matrix)
     return (contour₁, contour₂)
 end
 
-pi = 22//7
+pi = 22 // 7
 
 function equations()
     real_intrinsics::Matrix{Rational} = [
@@ -119,49 +119,73 @@ function equations()
     ]
 
     cameras_positions = [
-        [1, 1, 1],
-        [-1, 1, 1]
+        [10, 10, 10],
+        [-10, 10, 10]
     ]
+    # camera_rotations_raw = [
+    #     [271 // 12, 123 // 8, -11 // 16], # [3π / 4, 0, 5π / 4],
+    #     [-32 // 5, 1881 // 13, 271 // 12]  # [π / 3, 0, 3π / 4]
+    # ]
     camera_rotations_raw = [
-        [3pi//4, 0, 5pi//4],
-        [pi//3, 0, 3pi//4]
+        [271 / 12, 123 / 8, -11 / 16], # [3π / 4, 0, 5π / 4],
+        [-32 / 5, 1881 / 13, 271 / 12]  # [π / 3, 0, 3π / 4]
     ]
     camera_rotations = [
-        build_rotation_matrix(camera_rotations_raw[1]..., true),
-        build_rotation_matrix(camera_rotations_raw[2]..., true),
+        build_rotation_matrix(camera_rotations_raw[1]..., false),
+        build_rotation_matrix(camera_rotations_raw[2]..., false),
     ]
     display("Camera rotations")
     display(camera_rotations)
     display("-----------------------")
-    cameraMatrices = [
-        build_camera_matrix(real_intrinsics, camera_rotations[1], cameras_positions[1])
+    camera_matrices = [
+        build_camera_matrix(real_intrinsics, camera_rotations[1], cameras_positions[1]),
         build_camera_matrix(real_intrinsics, camera_rotations[2], cameras_positions[2])
     ]
     display("Camera matrices")
-    display(cameraMatrices)
+    display(camera_matrices)
     display("-----------------------")
     points_at_infinity = []
     for cylinder_index in 1:3
         push!(points_at_infinity, cylinders_transform_matrices[cylinder_index] * [0; 0; 1; 0])
     end
-    lines = zeros(Rational{Int}, 2, 3, 2, 4)
+    display("Points at infinity")
+    display(points_at_infinity)
+    display("-----------------------")
+    # lines = zeros(Rational{Int}, 2, 3, 2, 4)
+    lines = zeros(Float64, 2, 3, 2, 3)
     for camera_index in 1:2
         for cylinder_index in 1:3
             line1, line2 = get_cylinder_contours(
                 points_at_infinity[cylinder_index],
                 cameras_positions[camera_index],
-                cameraMatrices[camera_index]
+                camera_matrices[camera_index]
             )
-            lines[camera_index, cylinder_index, 1, :] = [line1; 1]
-            lines[camera_index, cylinder_index, 2, :] = [line2; 1]
+            lines[camera_index, cylinder_index, 1, :] = line1
+            lines[camera_index, cylinder_index, 2, :] = line2
         end
     end
     display("Lines")
+    x = -4000:1:1000
+    f = Figure()
+    ax = Axis(f[1, 1]; aspect=DataAspect())
+    xlims!(ax, -4000, 1000)
+    ylims!(ax, -4000, 1000)
+
     for camera_index in 1:2
         for cylinder_index in 1:3
-            display(lines[camera_index, cylinder_index, :, :])
+            for line_index in 1:2
+                display("Camera $(camera_index), Cylinder $(cylinder_index), Line $(line_index)")
+                line = lines[camera_index, cylinder_index, line_index, :]
+                display(line)
+                a = -line[1] / line[2]
+                b = -line[3] / line[2]
+                if camera_index == 1
+                    draw_lines!(ax, x, a .* x .+ b)
+                end
+            end
         end
     end
+    display(f)
     display("---------------------------")
     system_to_solve = []
     parameters::Vector{HomotopyContinuation.ModelKit.Variable} = []
@@ -171,7 +195,7 @@ function equations()
     intrinsic_variables = [
         fx skew cx;
         0 fy cy;
-        0 0 1;
+        0 0 1
     ]
 
     @var r1x, r1y, r1z, r2x, r2y, r2z
