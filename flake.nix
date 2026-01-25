6328a058eb2a0cc469bac438da76da196a47b902
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2026-01-25 13:13:27 +0000

    Update julia version

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         devShells.default = pkgs.mkShell {
           name = "julia-dev-shell";
           buildInputs = [
-            pkgs.julia
+            pkgs.julia_1_11
             pkgs.git
             pkgs.gnumake
             pkgs.python3
