c5ecb82afcb462d3a43759f9cd891bcbbc7a4485
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2026-03-03 20:41:43 +0000

    Revert file to state before 7fd0bac

--- src/CylindersBasedCameraResectioning.jl
+++ src/CylindersBasedCameraResectioning.jl
@@ -7,7 +7,7 @@ module CylindersBasedCameraResectioning
 
     using ..Cylinder: cylinder_from_center_axis_radius, points_at_infinity_dualquadrics
 	using ..Scene: SceneData, InstanceConfiguration, ParametersSolutionsPair, averaged_solution!, best_overall_solution!, best_overall_solution_by_steps!, best_intrinsic_rotation_translation_system_solution!, camera_from_solution, create_scene_instances_and_problems, scene_instances_and_problems_from_files, intrinsic_rotation_system_setup, intrinsic_rotation_translation_system_setup, plot_interactive_scene, plot_reconstructed_scene, split_intrinsic_rotation_parameters, plot_scene
-    using ..Camera: CameraProperties, CameraViewPair, lookat_quaternion
+    using ..Camera: CameraProperties, CameraViewPair
     using ..Geometry: get_view
 	using ..EquationSystems: stack_homotopy_parameters, variables_jacobian_rank, joint_jacobian_rank
     using ..EquationSystems.Problems: CylinderCameraContoursProblem, CylinderCameraContoursProblemValidationData
@@ -16,11 +16,9 @@ module CylindersBasedCameraResectioning
 	using ..Printing: print_camera_differences, print_relative_motion_errors
     using ..Homotopies: ParameterHomotopy as MyParameterHomotopy
     using ..IO: read_point_cloud_zup, read_cylinders_zup, read_camera_from_matrices, read_cylinder_line_views
-    using ..Utils: lines_clp_to_stack, get_view_CC_VP_P
-    using ..Homotopies: GeometricHomotopy
+    using ..Utils: lines_clp_to_stack
 
     using HomotopyContinuation, Observables, Random, Serialization
-    using LinearAlgebra, Rotations
 
     function main()
         intrinsic_configuration = IntrinsicParametersConfigurations.none
@@ -525,132 +523,40 @@ module CylindersBasedCameraResectioning
                 "cylinder_4",
             ],
             plot_3d = false,
-            use_all_lines = true,
         )
         intrinsic_configuration = problems[1].intrinsic_configuration
 
-        points_at_infinity = Array{Float64}(undef, 10, 3)
-        dualquadrics = Array{Float64}(undef, 10, 4, 4)
-        for (i, cylinder) in enumerate(scene.cylinders)
-            index = (i-1)*2 + 1
-            points_at_infinity[index, :] = cylinder.singular_point[1:3]
-            points_at_infinity[index+1, :] = cylinder.singular_point[1:3]
-            dualquadrics[index, :, :] = cylinder.dual_matrix
-            dualquadrics[index+1, :, :] = cylinder.dual_matrix
-        end
+        rotation_intrinsic_system, parameters = intrinsic_rotation_system_setup(problems; intrinsic_configuration)
 
-        equation_combinations = [1, 2, 3, 4, 5, 6, 7] # [1, 3, 5, 7, 9, 2, 4]
+        display(scene.figure)
 
-        rotation_intrinsic_system, parameters = intrinsic_rotation_system_setup(problems;
-            intrinsic_configuration,
-            minimization=false,
-            equation_combinations
+        solver = starts = nothing
+
+        solver, starts = solver_startsolutions(
+            rotation_intrinsic_system;
+            target_parameters = parameters,
+            start_system = :total_degree,
         )
 
-        offset = 3.0
-        offsetted_scene = SceneData()
-        offsetted_scene.figure = initfigure()
-        offsetted_scene.cylinders = scene.cylinders
-
-        offsetted_intrinsics = scene.instances[1].camera.intrinsic + [
-            (rand(Float64) * 50.0 - 25.0) 0.0 (rand(Float64) * 10.0 - 5.0);   # fₓ, skew, cₓ
-            0.0 (rand(Float64) * 50.0 - 25.0) (rand(Float64) * 10.0 - 5.0);   # 0, fᵧ, cᵧ
-            0.0 0.0 0.0                                                      # bottom row
-        ]
-
-        offsetted_scene.instances = map((instance) -> begin
-            offsetted_camera = deepcopy(instance.camera)
-            offsetted_camera.intrinsic = offsetted_intrinsics
-            random_direction = normalize(randn(3))
-            offsetted_camera.position = offsetted_camera.position + offset * random_direction
-            # rotation = lookat_quaternion(offsetted_camera.position, [0.0, 0.0, 0.0])
-            # offsetted_camera.quaternion_rotation = rotation
-            views = Array{Float64}(undef, 5, 2, 3)
-            for (i, cylinder) in enumerate(offsetted_scene.cylinders)
-                ll1, ll2 = get_view_CC_VP_P(cylinder.dual_matrix, cylinder.singular_point, offsetted_camera.matrix)
-                views[i, 1, :] = ll1
-                views[i, 2, :] = ll2
-            end
-            offsetted_instance = InstanceConfiguration()
-            offsetted_instance.camera = offsetted_camera
-            offsetted_instance.conics_contours = views
-            return offsetted_instance
-        end, scene.instances)
-
-        offsetted_problems::Vector{CylinderCameraContoursProblem} = []
-        validation_data = CylinderCameraContoursProblemValidationData(
-            Matrix{Float64}(undef, 0, 3),
-            Matrix{Float64}(undef, 0, 3),
-            Array{Float64}(undef, 0, 4, 4),
-        )
-        for (_, instance) in enumerate(scene.instances)
-            lines = lines_clp_to_stack(instance.conics_contours)
-            problem = CylinderCameraContoursProblem(
-                CameraProperties(),
-                lines,
-                lines,
-                points_at_infinity,
-                dualquadrics,
-                validation_data,
-                UInt8(intrinsic_configuration)
-            )
-            push!(offsetted_problems, problem)
-        end
-
-        rotation_intrinsic_system, offsetted_parameters = intrinsic_rotation_system_setup(offsetted_problems;
-            intrinsic_configuration,
-            equation_combinations
-        )
-
-        #region Computing original sol
-        rot = Rotations.params(QuatRotation(offsetted_scene.instances[1].camera.rotation_matrix))
-        rot = rot / rot[1]
-        rot1 = rot[2:4]
-        known_solution = [
-            offsetted_scene.instances[1].camera.intrinsic[1, 1] / 3000.0,
-            offsetted_scene.instances[1].camera.intrinsic[2, 2] / 3000.0,
-            offsetted_scene.instances[1].camera.intrinsic[1, 3] / 3000.0,
-            offsetted_scene.instances[1].camera.intrinsic[2, 3] / 3000.0,
-            rot1...,
-        ]
-        #endregion
-
-        display(evaluate(rotation_intrinsic_system, known_solution, offsetted_parameters))
-
-        # return
-
-        monodromy_solutions = solutions(
-            monodromy_solve(
-                rotation_intrinsic_system,
-                [known_solution],
-                offsetted_parameters;
-                show_progress=true,
-            )
-        )
-
-        display("Monodromy end")
-
-        # display(scene.figure)
-
-        result = solve(
-            GeometricHomotopy(
-                rotation_intrinsic_system,
-                start_parameters=offsetted_parameters,
-                target_parameters=parameters,
-            ),
-            monodromy_solutions;
-            show_progress=true
-        )
-        @info result
-
+        chunk_size = 500000
+        numberof_start_solutions = length(starts)
+        display("Number of start solutions: $numberof_start_solutions. Number of iterations needed: $(ceil(Int, numberof_start_solutions / chunk_size))")
         solution_error = Inf
-        solution_error, _ = best_overall_solution_by_steps!(
-            result,
-            problems;
-            start_error=solution_error,
-            intrinsic_configuration,
-            scene,
-        )
+        for start in Iterators.partition(starts, chunk_size)
+            result = solve(
+                solver,
+                start;
+            )
+            @info result
+
+            solution_error, _ = best_overall_solution_by_steps!(
+                result,
+                problems;
+                start_error=solution_error,
+                intrinsic_configuration,
+                # scene,
+            )
+        end
 
         display("Solution error: $solution_error")
 
@@ -857,7 +763,7 @@ module CylindersBasedCameraResectioning
 
         plot_reconstructed_scene(scene, problems; plot_3d = true, raw_3d = false)
 
-        # save_2d_figures("assets/test_scenes/hot_pipes/figures/", scene, problems; scene_file_path = "./assets/test_scenes/hot_pipes/scene.json", raw_3d = true)
+        save_2d_figures("assets/test_scenes/hot_pipes/figures/", scene, problems; scene_file_path = "./assets/test_scenes/hot_pipes/scene.json", raw_3d = true)
 
         display(scene.figure)
     end
